a6c693f4e7d64bb6929c6829194d8c90f27e8ecd
Roger Taracha <rojtaracha@gmail.com>
2017-08-14 15:09:11 +0300

    Writing Your First Component. [+] Create StorePicker Component using react package. [+] Mount it to page using react-dom package.

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+/*
+    StorePicker Component
+    This will let is make <StorePicker/>
+*/
+
+var StorePicker = React.createClass({
+
+  render : function() {
+    return (
+      <p>Hello</p>
+    )
+  }
+
+});
+
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
